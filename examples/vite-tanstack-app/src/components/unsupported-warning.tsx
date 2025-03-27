db7aad6145bb254ac89550c3f9f674f72c08ba17
David Steiner <david_j_steiner@yahoo.co.nz>
2025-03-27 19:00:44 +0100

    Display warning on unsupported devices

---
+++ examples/vite-tanstack-app/src/components/unsupported-warning.tsx
@@ -0,0 +1,8 @@
+export function UnsupportedWarning() {
+  return (
+    <div>
+      <span className="font-bold">react-presentation-hook</span> does not work
+      on mobiles and tablets.
+    </div>
+  );
+}
--- examples/vite-tanstack-app/src/routes/__root.tsx
+++ examples/vite-tanstack-app/src/routes/__root.tsx
@@ -1,22 +1,32 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+
 import { lazy } from "react";
+import { isMobile, isTablet } from "react-device-detect";
+
+import { UnsupportedWarning } from "@/components/unsupported-warning";
 
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
     ? () => null
     : lazy(() =>
-      import("@tanstack/react-router-devtools").then((res) => ({
-        default: res.TanStackRouterDevtools,
-      }))
-    );
+        import("@tanstack/react-router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
 
 export const Route = createRootRoute({
-  component: () => (
+  component: Root,
+});
+
+function Root() {
+  const isSupportedDevice = !(isMobile || isTablet);
+
+  return (
     <div className="max-w-3xl mx-auto h-screen py-8">
       <div className="h-full px-4 py-8 flex flex-col gap-4 bg-gradient-to-b from-gray-50 to-gray-100 rounded-lg">
-        <Outlet />
+        {isSupportedDevice ? <Outlet /> : <UnsupportedWarning />}
       </div>
       <TanStackRouterDevtools />
     </div>
-  ),
-});
+  );
+}
